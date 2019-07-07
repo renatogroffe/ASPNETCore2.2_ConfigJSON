df6106355748f9acb642670d00401e979eeda499
Renato Groffe <renato.groffe@yahoo.com.br>
2019-07-07 14:09:18 -0300

    ASP.NET Core 2.2 + Leitura configurações appsettings.json
    ASP.NET Core 2.2 + Leitura configurações appsettings.json

---
+++ APIDadosBrasil/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using APIDadosBrasil.Models;
+
+namespace APIDadosBrasil
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+           var dadosPais = new DadosPais();
+
+            new ConfigureFromConfigurationOptions<DadosPais>(
+                Configuration.GetSection("DadosPais"))
+                    .Configure(dadosPais);
+
+            services.AddSingleton(dadosPais);
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
